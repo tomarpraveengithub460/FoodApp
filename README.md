944926f551d145a04c27144e2c94435a05fe9ac0
tomarpraveengithub460 <praveentomar460@gmail.com>
2024-06-10 18:15:15 +0530

    Adding Button

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # FoodApp
 Building a food Delivery App 
-
-Note that : 
-Whenever there is static/hard coded data then serve that data in separate file.
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,51 @@
+// import RestaurentCard from "./RestaurentCard";
+// import resList from "../../data";
+
+// const Body = () => {
+// return (
+//     <div className="body">
+//         <div className="filter">
+//             <button className="filter-btn" onClick={() => {
+//                 <filterRating/>;
+//             }}>Top Rated Restaurent</button>
+//         </div>
+//         <div className="res-container">
+//             {
+//                 resList.map(restaurent => <RestaurentCard key={restaurent.data.id} resData={restaurent} />)
+//             }
+//         </div>
+//     </div>
+// )
+// }
+// export default Body; 
+
+
+
 import RestaurentCard from "./RestaurentCard";
 import resList from "../../data";
-
+import { useState } from "react";
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search">Search</div>
-            <div className="res-container">
-                {
-                    resList.map(restaurent => <RestaurentCard key={restaurent.data.id} resData={restaurent} /> )
-                    //never use index as the key
-                }
-            </div>
+    const [filteredList,setFilteredList]=useState(resList); //Initialization Line
+    function filterRes(){
+        let rest=filteredList.filter((res)=>{
+            return res.data.avgRating>=4.4;
+        });
+        setFilteredList(rest);
+        console.log(rest);
+    }
+return (
+    <div className="body">
+        <div className="filter">
+            <button className="filter-btn" onClick={() => {
+                filterRes();
+            }}>Top Rated Restaurent</button>
         </div>
-    )
+        <div className="res-container">
+            {
+                filteredList.map(restaurent => <RestaurentCard key={restaurent.data.id} resData={restaurent} />)
+            }
+        </div>
+    </div>
+)
 }
 export default Body; 
